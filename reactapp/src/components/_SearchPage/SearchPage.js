b7a3d9794d4f45369f223f7c8b6a47bb14c20c55
alejandrok93 <alejandrok93@gmail.com>
2018-11-26 17:55:39 -0600

    Removed componentDidMount action call from Search Page

--- reactapp/src/components/_SearchPage/SearchPage.js
+++ reactapp/src/components/_SearchPage/SearchPage.js
@@ -1,51 +1,49 @@
 // Dependencies
-import React, { Component } from "react";
+import React, { Component } from 'react';
 // import { Route } from "react-router-dom";
-import { fetchSearchResults } from "../../actions";
-import { connect } from "react-redux";
-import "./SearchPage.css";
+import { fetchSearchResults } from '../../actions';
+import { connect } from 'react-redux';
+import './SearchPage.css';
 
 //Import components
-import { SearchBar, ProjectTile } from "../../components";
+import { SearchBar, ProjectTile } from '../../components';
 
 class SearchPage extends Component {
-  // constructor() {
-  //   super();
-  // } // useless constructor
+	// constructor() {
+	//   super();
+	// } // useless constructor
 
-  componentDidMount() {
-    this.props.fetchSearchResults();
-  }
-  handleChange = e => {
-    console.log(e.target.value);
-  };
+	componentDidMount() {}
+	handleChange = e => {
+		console.log(e.target.value);
+	};
 
-  render() {
-    console.log("hellooooo");
-    return (
-      <div className="search-page-container">
-        <SearchBar handleChange={this.handleChange} />
+	render() {
+		console.log(this.props.projects);
+		return (
+			<div className="search-page-container">
+				<SearchBar handleChange={this.handleChange} />
 
-        <div className="search-options" />
-        <div className="search-results">
-          <h1>Search results</h1>
-          {this.props.projects.map(project => (
-            <ProjectTile project={project} />
-          ))}
-        </div>
-      </div>
-    );
-  }
+				<div className="search-options" />
+				<div className="search-results">
+					<h1>Search results</h1>
+					{this.props.projects.map(project => (
+						<ProjectTile project={project} />
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  console.log(state);
-  return {
-    projects: state.searchReducer.projects
-  };
+	console.log(state);
+	return {
+		projects: state.searchReducer.projects
+	};
 };
 
 export default connect(
-  mapStateToProps,
-  { fetchSearchResults }
+	mapStateToProps,
+	{ fetchSearchResults }
 )(SearchPage);
