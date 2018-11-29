91294bde41fcd79c89219c8847c81092072ca13b
David Benavidez <benavidez_david13@yahoo.com>
2018-11-29 12:50:57 -0800

    working on routes and model for categories

--- server/models/categoryModel.js
+++ server/models/categoryModel.js
@@ -2,18 +2,17 @@ const db = require('../config/dbConfig');
 
 module.exports = {
 	addCategory,
-	editCategory,
 };
 
-function addCategory(user_id, project_id, category) {
+function addCategory( project_id, category) {
 	return db('projects')
-		.where({ user_id, project_id })
+		.where({ project_id  })
 		.first()
 		.then(project => {
 			if (project) {
 				return db('categories')
 					.returning('category_id')
-					.insert({ ...category, project_id })
+					.insert({ ...category, project_id  })
 					.then(([id]) => {
 						if (id) {
 							return db('projects')
@@ -26,28 +25,6 @@ function addCategory(user_id, project_id, category) {
 		});
 }
 
-// I feel like this could be more elegant.
-function editCategory(user_id, project_id, post_id, changes) {
-	return db('projects')
-		.where({ user_id, project_id })
-		.first()
-		.then(post => {
-			if (post) {
-				return db('posts')
-					.where({ project_id, post_id })
-					.returning('post_id')
-					.update(changes)
-					.then(ids => ids.length)
-					.then(count => {
-						if (count) {
-							return db('projects')
-								.where({ project_id })
-								.returning('project_id')
-								.update({ last_updated: db.fn.now() })
-								.then(ids => ids.length);
-						} else return undefined; // no post by that id
-					});
-			} else return undefined; // no project by that id || wrong author
-		});
-}
+
+
 
--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -31,30 +31,4 @@ router.post('/',  function(req, res, next) {
 });
 
 
-
-// update category by id
-router.put('/:post_id', ensureLoggedIn, function(req, res, next) {
-	const { user_id, project_id, img_url, text } = req.body;
-	const { post_id } = req.params;
-
-	if (!img_url && !text) {
-		return res.status(422).json({ error: 'Post cannot be empty.' });
-	} else {
-		const changes = { img_url, text };
-		db.editPost(user_id, project_id, post_id, changes)
-			.then(count => {
-				if (count) {
-					res.status(200).json(count);
-				} else {
-					// 404 if post doesn't exist ?
-					// 403 if post exists but user is not author
-					res.status(403).json({ error: 'Not authorized.' });
-				}
-			})
-			.catch(err => {
-				res.status(500).json(err);
-			});
-	}
-});
-
 module.exports = router;
