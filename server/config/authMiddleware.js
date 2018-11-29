a63cc667e6f2a18d05ef0da3076c13335073f450
Max <maxkajiwara@gmail.com>
2018-11-29 09:28:14 -0800

    Update auth middleware to handle missing parameters

--- server/config/authMiddleware.js
+++ server/config/authMiddleware.js
@@ -1,21 +1,26 @@
-const authDB = require('../models/authModel')
+const authDB = require('../models/authModel');
 
-function authenticate(req, res, next) {
-  const { user_id } = req.body;
-  //get user_id from user object
-  console.log(req.body);
-  let sub = req.user._json.sub.split("|");
-  let auth_id = sub[1];
+function authorize(req, res, next) {
+	const { user_id } = req.body;
 
-  authDB
-    .getAuthID(user_id)
-    .then(db_auth_id => {
-      if (db_auth_id === auth_id) {
-        next();
-      } else {
-        res.status(403).json({ error: 'Not authorized.' });
-      }
-    });
+	if (!user_id) {
+		res.status(403).json({ error: 'Not logged in.' });
+	}
+
+	if (!req.user._json) {
+		res.status(403).json({ error: 'No login cookie.' });
+	}
+
+	let sub = req.user._json.sub.split('|');
+	let auth_id = sub[1];
+
+	authDB.getAuthID(user_id).then(db_auth_id => {
+		if (db_auth_id === auth_id) {
+			next();
+		} else {
+			res.status(403).json({ error: 'Not authorized.' });
+		}
+	});
 }
 
-module.exports = authenticate;
+module.exports = authorize;
