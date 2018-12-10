26bcebd575112928e82d91162268598888aa9446
Max <maxkajiwara@gmail.com>
2018-12-10 03:24:16 -0800

    Update header to use <Link> instead of href

--- reactapp/src/components/Header/Header.js
+++ reactapp/src/components/Header/Header.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { SearchBar, Nav } from '../index';
@@ -27,32 +27,28 @@ const Logo = styled.img`
 	cursor: pointer;
 `;
 
-class Header extends Component {
-	state = {};
-
-	render() {
-		return (
-			<HeaderContainer>
-				{this.state.redirect && <Redirect push to={this.state.redirect} />}
-				<HeaderContainerWraper>
+const Header = props => {
+	return (
+		<HeaderContainer>
+			<HeaderContainerWraper>
+				<Link to="/">
 					<Logo
 						style={{ width: '60px', height: '60px', margin: '0 20px' }}
 						src="https://ratemydiy.s3.amazonaws.com/1543872216210"
 						alt="LOGO"
-						onClick={() => this.setState({ redirect: '/' })}
 					/>
+				</Link>
 
-					<HeaderSearchContainer>
-						<SearchBar
-							handleChange={this.props.handleChange}
-							handleSearch={this.props.handleSearch}
-						/>
-					</HeaderSearchContainer>
-					<Nav />
-				</HeaderContainerWraper>
-			</HeaderContainer>
-		);
-	}
-}
+				<HeaderSearchContainer>
+					<SearchBar
+						handleChange={props.handleChange}
+						handleSearch={props.handleSearch}
+					/>
+				</HeaderSearchContainer>
+				<Nav />
+			</HeaderContainerWraper>
+		</HeaderContainer>
+	);
+};
 
 export default Header;
