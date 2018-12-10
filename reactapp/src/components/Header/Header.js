a1dc756d6b0c0d4fd2706ca80b1332190cab5605
Max <maxkajiwara@gmail.com>
2018-12-10 03:05:36 -0800

    Keep the header from bouncing when page loads

--- reactapp/src/components/Header/Header.js
+++ reactapp/src/components/Header/Header.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
-
 
 import { SearchBar, Nav } from '../index';
 
 const HeaderContainer = styled.div`
 	width: 100%;
+	height: 76px;
 	z-index: 999;
 	position: fixed;
 	background-color: #232a34;
@@ -15,36 +16,43 @@ const HeaderContainerWraper = styled.div`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
-	margin: 10px 0;
 `;
 
 const HeaderSearchContainer = styled.div`
 	width: 50%;
-
 	margin: 0 20px;
 `;
 
-const Header = props => {
-	return (
-		<HeaderContainer>
-			<HeaderContainerWraper>
-				<a href="/">
-					<img
+const Logo = styled.img`
+	cursor: pointer;
+`;
+
+class Header extends Component {
+	state = {};
+
+	render() {
+		return (
+			<HeaderContainer>
+				{this.state.redirect && <Redirect push to={this.state.redirect} />}
+				<HeaderContainerWraper>
+					<Logo
 						style={{ width: '60px', height: '60px', margin: '0 20px' }}
 						src="https://ratemydiy.s3.amazonaws.com/1543872216210"
 						alt="LOGO"
+						onClick={() => this.setState({ redirect: '/' })}
 					/>
-				</a>
-				<HeaderSearchContainer>
-					<SearchBar
-						handleChange={props.handleChange}
-						handleSearch={props.handleSearch}
-					/>
-				</HeaderSearchContainer>
-				<Nav />
-			</HeaderContainerWraper>
-		</HeaderContainer>
-	);
-};
+
+					<HeaderSearchContainer>
+						<SearchBar
+							handleChange={this.props.handleChange}
+							handleSearch={this.props.handleSearch}
+						/>
+					</HeaderSearchContainer>
+					<Nav />
+				</HeaderContainerWraper>
+			</HeaderContainer>
+		);
+	}
+}
 
 export default Header;
