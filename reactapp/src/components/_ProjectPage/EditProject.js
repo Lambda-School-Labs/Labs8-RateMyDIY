29ce607a2509baeb8346df046ff09544e259a8dc
michaelagard <mdagard@gmail.com>
2018-12-16 19:28:25 -0500

    reverted some redux related changes to get project images to upload

--- reactapp/src/components/_ProjectPage/EditProject.js
+++ reactapp/src/components/_ProjectPage/EditProject.js
@@ -1,6 +1,7 @@
 // Dependencies
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 // Components
 import { ConfirmModal } from '../../components';
 import StarRatings from 'react-star-ratings';
@@ -19,40 +20,94 @@ class EditProject extends Component {
 		text: '',
 		categories: [],
 		selectedFile: null,
-		// file: null
+		uploadingProjectImage: false
 	};
 
 	// This stores the project image file recieved in the ReactFileReader form data  
-	projectImageSelector = (event) => {
-		const file = URL.createObjectURL(event.target.files[0])
-		this.setState(() => ({ img_url: file }))
+	singleFileChangedHandler = event => {
+		this.setState({
+			selectedFile: event.target.files[0]
+		});
 	};
 
-	projectImageUploader = event => {
+	singleFileUploadHandler = event => {
 		event.preventDefault();
-
+		const data = new FormData();
 		// If file selected
-		if (this.state.selectedFile) {
-
-			const data = new FormData();
-
-			const headerData = {
-				headers: {
-					accept: 'application/json',
-					'Accept-Language': 'en-US,en;q=0.8',
-					'Content-Type': `multipart/form-data; boundary=${data._boundary}`
-				}
-			}
-
-			console.log("FILE SELECTED " + this.state.selectedFile);
-
+		if (this.state.selectedFile && !this.state.uploadingProjectImage) {
 			data.append(
 				'image',
 				this.state.selectedFile,
 				this.state.selectedFile.name
 			);
+			this.setState({
+				uploadingProjectImage: true
+			});
+			axios
+				.post(
+					(process.env.REACT_APP_BACKEND || 'http://localhost:5000') +
+					`/api/projects/image-upload`,
+					data,
+					{
+						onUploadProgress: progressEvent => {
+							console.log(
+								'Upload Progress:' +
+								Math.round(
+									(progressEvent.loaded / progressEvent.total) * 100
+								) +
+								'%'
+							);
+						}
+					},
+					{
+						headers: {
+							accept: 'application/json',
+							'Accept-Language': 'en-US,en;q=0.8',
+							'Content-Type': `multipart/form-data; boundary=${data._boundary}`
+						}
+					}
+				)
+				.then(response => {
+					if (200 === response.status) {
+						// If file size is larger than expected.
+						if (response.data.error) {
+							if ('LIMIT_FILE_SIZE' === response.data.error.code) {
+								// this.ocShowAlert("Max size: 2MB", "red");
+								this.setState({
+									uploadingProjectImage: false
+								});
+							} else {
+								this.setState({
+									uploadingProjectImage: false
+								});
+								console.log(response.data.location);
+								// If not the given file type
+								// this.ocShowAlert(response.data.error, "red");
+								console.log(response.data.path);
+							}
+						} else {
+							// Success
+							let fileName = response.data;
 
-			this.props.updateProjectImage(data, headerData, value => this.setState({ img_url: value }))
+							let photo = response.data.location;
+							this.setState({
+								img_url: photo,
+								uploadingProjectImage: false
+							});
+							console.log('filedata', fileName);
+
+							console.log('photo', photo);
+
+							//   this.ocShowAlert("File Uploaded", "#3089cf");
+						}
+					} else {
+						console.log('error');
+					}
+				})
+				.catch(error => {
+					// If another error
+					console.log('error');
+				});
 		}
 	};
 
@@ -64,6 +119,9 @@ class EditProject extends Component {
 	// Submit changes
 	submitProjectChanges = event => {
 		event.preventDefault();
+		if (this.props.selectedFile && !this.state.uploadingProjectImage) {
+			this.singleFileUploadHandler(event);
+		}
 		this.props.updateProject(
 			this.props.project.project_id,
 			{
@@ -142,7 +200,7 @@ class EditProject extends Component {
 					</ProjectNameAndAuthorContainer>
 					<UploadImageButtonTemp
 						className="btn btn-info"
-						onClick={this.projectImageUploader}
+						onClick={this.singleFileUploadHandler}
 						disabled={this.props.updatingProject || this.props.gettingProject}
 					>
 						Upload Image (temp)
@@ -152,7 +210,7 @@ class EditProject extends Component {
 					<ProjectPictureHiddenInput
 						type="file"
 						id="project_picture_input"
-						onChange={this.projectImageSelector}
+						onChange={this.singleFileChangedHandler}
 					/>
 					<ProjectPictureUploadLabel
 						htmlFor="project_picture_input"
@@ -195,7 +253,7 @@ class EditProject extends Component {
 					</SubmitLink>
 				</EditProjectOptionsContainer>
 
-				{(this.props.updatingProject || this.props.gettingProject) && (
+				{(this.props.updatingProject || this.props.gettingProject || this.state.uploadingProjectImage) && (
 					<StatusMessage small>Updating project...</StatusMessage>
 				)}
 				{this.props.updatingProjectError && (
